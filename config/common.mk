keepQASSA/vendor_qassa

vendor: replace test-keys with our keys
This will automatically sign output file so no need to signing manually

config/common.mk
@@ -239,3 +239,6 @@ ifneq ($(TARGET_APERTURE_OPTOUT),true)
 PRODUCT_PACKAGES += \
     Aperture
 endif
+
+# Keys for sign
+$(call inherit-product-if-exists, vendor/qassa/signing/keys.mk)

signing/keys.mk
@@ -1,0 +1,1 @@
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/qassa/signing/releasekey

paiskomom/android_vendor_resurrection

config/common.mk
@@ -204,3 +204,6 @@ PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 endif
+
+# Keys for sign
+$(call inherit-product-if-exists, vendor/rr/signing/keys.mk)

signing/keys.mk
@@ -1,0 +1,1 @@
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/rr/signing/releasekey